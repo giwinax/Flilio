013c7894d0ed611b0bc0f979ecc05509014715f1
Stanislav <giwinax@gmail.com>
2022-03-04 11:37:25 +0300

    Initial Commit

---
+++ Filio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Filio
+//
+//  Created by s b on 04.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
